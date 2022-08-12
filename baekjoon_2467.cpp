8f980ed356f359776ddcc0cf78b494d42819c41e
kim <98096810+SorryKim@users.noreply.github.com>
2022-08-12 17:16:50 +0900

    용액 / baekjoon_2467.cpp

---
+++ baekjoon_2467.cpp
@@ -0,0 +1,59 @@
+// 용액 / baekjoon_2467.cpp
+// https://www.acmicpc.net/problem/2467
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int N;
+long long arr[100001];
+long long sum = 1000000000001;
+long long temp[2];
+
+void input() {
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+}
+
+void solve() {
+
+    int left = 0;
+    int right = N - 1;
+
+    while (left < right) {
+        long long value = arr[left] + arr[right];
+ 
+        if (abs(sum) > abs(value)) {
+            sum = value;
+            temp[0] = arr[left];
+            temp[1] = arr[right];
+        }
+            
+        if (value > 0)
+            right--;
+        else if (value < 0)
+            left++;
+        else {
+            temp[0] = arr[left];
+            temp[1] = arr[right];
+            break;
+        }
+
+    }
+    
+    cout << temp[0] << " " << temp[1] << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solve();
+
+}
