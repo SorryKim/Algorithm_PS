26d9f2dbc412883da4d38d84c5cc2c5b077d5394
kim <98096810+SorryKim@users.noreply.github.com>
2022-09-01 17:10:19 +0900

    가운데를 말해요 / baekjoon_1655.java

---
+++ baekjoon_1655.java
@@ -0,0 +1,52 @@
+// 가운데를 말해요 / baekjoon_1655.java
+// https://www.acmicpc.net/problem/1655
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static PriorityQueue<Integer> pq1, pq2;
+	public static int N;
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		
+		// 수를 절반씩 나누어 저장, pq1은 오름차순, pq2는 내림차순
+		pq1 = new PriorityQueue<>(Collections.reverseOrder());
+		pq2 = new PriorityQueue<>();
+
+		for(int i = 0; i < N; i++) {
+
+			int num = Integer.parseInt(br.readLine());
+			
+			if(pq1.isEmpty() || num <= pq1.peek())
+				pq1.add(num);
+			else
+				pq2.add(num);
+			
+			while(pq1.size() >= pq2.size()) {
+				int temp = pq1.poll();
+				pq2.add(temp);
+			}
+			
+			if(pq2.size() > pq1.size()) {
+				int temp = pq2.poll();
+				pq1.add(temp);
+			}
+
+			bw.write(Integer.toString(pq1.peek()) + "\n");
+		}
+
+		br.close();
+		bw.close();
+	}
+
+}
