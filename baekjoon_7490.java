7acff0170bc65f4ae8642af6d832875f912f73f8
Kim, Jaeseong <06zzkimzz06@gmail.com>
2022-11-17 22:20:49 +0900

    풀이완료
    백트래킹 / 문자열

--- baekjoon_7490.java
+++ baekjoon_7490.java
@@ -3,87 +3,94 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
 
-
 	static int N;
-	static int result = 0;
-	static String[] arr;
-	// 입력 받기
-	static void Input() throws Exception {
+	static char[] arr;
+	static ArrayList<String> result;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	// 연산자 배열을 int값으로 반환하는 메소드
+	static int calc(char[] arr) {
 
+		int sum = 0;
 
-		br.close();
+		for (int i = N; i > 1; i--) {
 
-	}
+			int num = i;
+			// +인 경우
+			if (arr[i - 1] == '+')
+				sum += num;
+			// -인 경우
+			else if (arr[i - 1] == '-')
+				sum -= num;
+			// 공백인 경우
+			else {
+				
+				int result = i;
+				int cnt = 1;
+				while (arr[i - 1] == ' ') {
+					i--;
+					result += i * (int) Math.pow(10, cnt++);
+				}
+				if (i - 1 >= 1 && arr[i - 1] == '-') {
+					sum -= result;
+				} 
+				else {
+					sum += result;
+				}
+			}
+		}
+		
+		if (arr[1] != ' ') {
+			sum += 1;
+		}
 
-	static int calc(String str) {
-
-		str = str.replaceAll(" ", " ");
-
-		String[] num = str.split("+|-");
-		String[] operator = str.split("");
-		return 1;
+		return sum;
 	}
 
 	static void DFS(int depth) {
 
-		String[] cmd = { "+", "-", " " };
-
-		if(depth == N - 1) {
-			String str = "1";
-			for(int i = 0; i < N; i++) {
-				str += arr[i] + Integer.toString( + 2);
+		if (depth == N) {
+			if (calc(arr) == 0) {
+				String str = "1";
+				for(int i = 1; i < N; i++) {
+					str += arr[i];
+					str += Integer.toString(i + 1);
+				}
+				result.add(str);
 			}
-
 			return;
 		}
-
-		for(int i = 0; i < 3; i++) {
-			arr[depth] = cmd[i];
+		
+		char[] op = { ' ', '+', '-' };
+		for (int i = 0; i < 3; i++) {
+			arr[depth] = op[i];
 			DFS(depth + 1);
 		}
 	}
 
 	static void Solve() {
 
-		arr = new String[N - 1];
-
-
+		arr = new char[N];
+		result = new ArrayList<>();
+		DFS(1);
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println(result.get(i));
+		}
 	}
 
-
 	public static void main(String[] args) throws Exception {
-
-		/*
 		int t;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		t = Integer.parseInt(br.readLine());
 
-		for(int i = 0; i < t; i++) {
+		for (int i = 0; i < t; i++) {
 			N = Integer.parseInt(br.readLine());
-			result = 0;
 			Solve();
+			System.out.println();
 		}
-
 		br.close();
-		*/
-
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String str = "121+232-1+2 2-3";
-		str.replaceAll("\\p{Z}", "");
-		System.out.println(str);
-		String[] num = str.split("\\+|-");
-		String[] operator = str.split("[1-9]+");
-
-
-		System.out.println(Arrays.toString(num));
-		System.out.println(Arrays.toString(operator));
 	}
-
 }
