2c0eb7549359d4235fe5fe210c8720c4e2360b56
kim <98096810+SorryKim@users.noreply.github.com>
2024-01-31 16:27:46 +0900

    [level 0] Title: 문자열 돌리기, Time: 2.54 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    
+    for(auto a : str){
+        cout << a << "\n";
+    }
+    return 0;
+}
