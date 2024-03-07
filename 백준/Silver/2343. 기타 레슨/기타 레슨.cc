31d72484c0aa8cc61a65eaad7549bc4c6dc231ba
kim <98096810+SorryKim@users.noreply.github.com>
2024-03-07 16:55:39 +0900

    [Silver I] Title: 기타 레슨, Time: 12 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,67 @@
+// 기타 레슨 / baekjoon_2343.cpp
+// https://www.acmicpc.net/problem/2343
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<long long> v;
+long long low, high;
+
+void Input() {
+
+    cin >> N >> M;
+    low, high = 0;
+
+    for (int i = 0; i < N; i++) {
+        long long num;
+        cin >> num;
+        v.push_back(num);
+        high += num;
+        low = max(low, num);
+    }
+
+}
+
+void Solve() {
+
+    // 강의의 순서가 바뀌면 안됨
+    //
+    // sort(v.begin(), v.end());
+
+    while (low <= high) {
+
+        long long mid = (low + high) / 2;
+        long long cnt = 0;
+        long long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (sum + v[i] > mid) {
+                sum = 0;
+                cnt++;
+            }
+            sum += v[i];
+        }
+
+        if (sum != 0)
+            cnt++;
+
+        if (cnt <= M)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    cout << low << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Input();
+    Solve();
+}
