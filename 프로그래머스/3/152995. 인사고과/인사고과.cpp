b57b94d1b2e57d3aa02e97e86b40d9b5e990903e
kim <98096810+SorryKim@users.noreply.github.com>
2024-05-02 10:08:33 +0900

    [level 3] Title: 인사고과, Time: 186.47 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -4,42 +4,39 @@
  
 using namespace std;
  
+
+bool cmp(const vector<int>& a, const vector<int>& b){
+    
+    return (a[0] + a[1]) > (b[0] + b[1]);    
+}
+
+
 int solution(vector<vector<int>> scores) {
     int rank = 1;
-    int sum = scores[0][0] + scores[0][1];
     
-    // 점수 내림차순 정렬
-    auto comp = [](vector<int> v1, vector<int> v2) 
-    {
-        return (v1[0]+v1[1]) > (v2[0]+v2[1]);
-    };
     
-    sort(scores.begin()+1, scores.end(), comp);
+    sort(scores.begin()+1, scores.end(), cmp);
     
     // 완호 위치 구하기
     for(int i = 1; i < scores.size(); ++i)
     {
        
-        if(scores[0][0] < scores[i][0] 
-          && scores[0][1] < scores[i][1])
+        if(scores[0][0] < scores[i][0] && scores[0][1] < scores[i][1])
             return -1;
         
         int score = scores[i][0] + scores[i][1];
+        int sum = scores[0][0] + scores[0][1];
+        
         if(sum < score)
             rank++;
         else 
             break;
     }
     
-
     int range = rank;
-    for(int i = 2; i < range; i++)
-    {
-        for(int j = 1; j < i; j++)
-        {        
-            if(scores[i][0] < scores[j][0] 
-               && scores[i][1] < scores[j][1])
-            {
+    for(int i = 2; i < range; i++){
+        for(int j = 1; j < i; j++){        
+            if(scores[i][0] < scores[j][0] && scores[i][1] < scores[j][1]){
                 rank--;
                 break;
             }
