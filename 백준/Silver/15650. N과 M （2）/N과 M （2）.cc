f68d640ce962cf5b5e374b94e461da6b964ae37c
kim <98096810+SorryKim@users.noreply.github.com>
2024-06-03 10:31:37 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -9,7 +9,7 @@ int N, M;
 bool visited[10] = { false, };
 int arr[10] = { 0, };
 
-void DFS(int cnt) {
+void DFS(int n, int cnt) {
 
 	if (cnt == M) {
 		for (int i = 0; i < M; i++) {
@@ -19,21 +19,10 @@ void DFS(int cnt) {
 		return;
 	}
 
-	for (int i = 1; i <= N; i++) {
-		if (cnt == 0 && visited[i] == false) {
-			visited[i] = true;
-			arr[cnt] = i;
-			DFS(cnt + 1);
-			visited[i] = false;
-		}
-		else if (visited[i] == false) {
-			if (arr[cnt - 1] < i) {
-				visited[i] = true;
-				arr[cnt] = i;
-				DFS(cnt + 1);
-				visited[i] = false;
-			}
-		}
+	for (int i = n; i <= N; i++) {
+		
+		arr[cnt] = i;
+		DFS(i + 1, cnt + 1);
 	}
 }
 
@@ -42,7 +31,7 @@ int main() {
     
     cin >> N >> M;
 	
-	DFS(0);
+	DFS(1, 0);
 
     return 0;
 }
