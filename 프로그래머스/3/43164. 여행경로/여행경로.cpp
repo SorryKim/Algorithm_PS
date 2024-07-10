ed82cfe5ac845bab5262bd0df4f7bf2e1ac3d04e
kim <98096810+SorryKim@users.noreply.github.com>
2024-07-10 16:29:33 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+bool cmp(string s1, string s2){
+    return s1 > s2;
+}
+
 void DFS(const string& now, unordered_map<string, vector<string>>& adj, vector<string>& route) {
     
     while (!adj[now].empty()) {
@@ -29,7 +33,7 @@ vector<string> solution(vector<vector<string>> tickets) {
     
     // 각 노드의 인접 리스트를 사전순으로 정렬
     for (auto& [key, value] : adj) {
-        sort(value.begin(), value.end(), greater<string>());
+        sort(value.begin(), value.end(), cmp);
     }
     
     
