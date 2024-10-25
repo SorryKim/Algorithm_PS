35e18cb86155235b3f078a5e5fc98727a924786a
kim <98096810+SorryKim@users.noreply.github.com>
2024-10-25 11:43:58 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+   
+    int N;
+    cin >> N;
+
+    cout << pow(2, N);
+    return 0;   
+}
