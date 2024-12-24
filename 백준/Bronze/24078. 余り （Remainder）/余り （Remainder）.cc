97594ea70e2e00c2d575d1d6b24ff528b89df046
kim <98096810+SorryKim@users.noreply.github.com>
2024-12-24 14:55:07 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int num;
+    cin >> num;
+    cout << num % 21;
+    
+    return 0;
+}
