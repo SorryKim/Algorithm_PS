0851495ade760c53db3a9397f0007f77f070c774
kim <98096810+SorryKim@users.noreply.github.com>
2025-02-24 20:32:56 +0900

    [Gold II] Title: 트리, Time: 1136 ms, Memory: 62792 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -5,61 +5,53 @@ import java.util.*;
 public class Main {
 
     static int N;
-    static int[] preorder, inorder;
+    static int[] preOrder, inOrder;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        for (int test = 0; test < T; test++) {
+        for (int TC = 0; TC < T; TC++) {
 
             N = Integer.parseInt(br.readLine());
-            preorder = new int[N + 1];
-            inorder = new int[N + 1];
+            preOrder = new int[N + 1];
+            inOrder = new int[N + 1];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++)
-                preorder[i] = Integer.parseInt(st.nextToken());
+                preOrder[i] = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++)
-                inorder[i] = Integer.parseInt(st.nextToken());
+                inOrder[i] = Integer.parseInt(st.nextToken());
 
-            Build(1, N, 1, N);
+            DFS(1, N, 1, N);
             System.out.println();
         }
-
     }
 
-    static void Build(int preStart, int preEnd, int inStart, int inEnd) {
+    static void DFS(int preStart, int preEnd, int inStart, int inEnd) {
 
-        if (preStart > preEnd)
+        if (preStart > preEnd || inStart > inEnd)
             return;
 
-        // 현재 서브트리의 루트
-        int root = preorder[preStart];
-
-        int inRootIdx = 0;
-
+        int root = preOrder[preStart];
+        int inRoot = 0;
         for (int i = 1; i <= N; i++) {
-            if (inorder[i] == root) {
-                inRootIdx = i;
+            if (inOrder[i] == root) {
+                inRoot = i;
                 break;
             }
         }
 
-        // 왼쪽 서브트리 크기
-        int leftSize = inRootIdx - inStart;
+        int left = inRoot - inStart;
 
         // Left
-        Build(preStart + 1, preStart + leftSize, inStart, inRootIdx - 1);
+        DFS(preStart + 1, preStart + left, inStart, inRoot - 1);
 
         // Right
-        Build(preStart + leftSize + 1, preEnd, inRootIdx + 1, inEnd);
+        DFS(preStart + left + 1, preEnd, inRoot + 1, inEnd);
 
         System.out.print(root + " ");
-
-        return;
     }
-
 }
