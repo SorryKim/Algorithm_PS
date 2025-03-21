a9655af3aedfeac23febead5917a35fc92e2803a
kim <98096810+SorryKim@users.noreply.github.com>
2025-03-22 08:55:28 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.reverse().toString();
+    }
+}
