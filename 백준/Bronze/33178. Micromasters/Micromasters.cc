a6ba312d43232a19760aac4b2d850d0145787423
kim <98096810+SorryKim@users.noreply.github.com>
2025-09-09 14:33:15 +0900

    [Bronze V] Title: Micromasters, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    cout << n / 10;
+}
