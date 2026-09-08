f6e53779ecd9147c4849ac6dee9ecc97fbbf6705
kim <98096810+SorryKim@users.noreply.github.com>
2026-09-08 16:32:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,35 +1,29 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <cmath>
+#include <climits>
+#include <iostream>
 
 using namespace std;
 
-vector<int> func(string& input) {
-    vector<int> result;
-    istringstream iss(input);
-    int number;
-
-    while (iss >> number) {
-        result.push_back(number);
-        iss.ignore();
-    }
-
-    return result;
-}
-
 string solution(string s) {
     string answer = "";
-    
-    vector<int> v = func(s);
-    
-    int maxV = v[0];
-    int minV = v[0];
-    
-    for(auto a : v){
-        maxV = max(maxV, a);
-        minV = min(minV, a);
+    stringstream ss(s);
+    vector<int> numbers;
+    int number;
+    while(ss >> number){
+        numbers.push_back(number);
     }
     
-    answer += to_string(minV) + " " + to_string(maxV);
+    int minV = INT_MAX;
+    int maxV = INT_MIN;
+    
+    for(int num : numbers){
+        minV = min(minV, num);
+        maxV = max(maxV, num);
+    }
+    
+    answer = to_string(minV) + " " + to_string(maxV);
     return answer;
 }
