54f1c7aecf3200480a1a0c90220e6368d929a088
kim <98096810+SorryKim@users.noreply.github.com>
2026-09-11 22:30:23 +0900

    [level 3] Title: 가장 먼 노드, Time: 8.09 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 14 MB, 시간: 10.42 ms
+메모리: 14 MB, 시간: 8.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 06일 15:03:23
+2026년 09월 11일 22:30:21
 
 ### 문제 설명
 
@@ -51,7 +51,7 @@
 
 <p>예제의 그래프를 표현하면 아래 그림과 같고, 1번 노드에서 가장 멀리 떨어진 노드는 4,5,6번 노드입니다.</p>
 
-<p><img src="https://grepp-programmers.s3.amazonaws.com/files/ybm/fadbae38bb/dec85ab5-0273-47b3-ba73-fc0b5f6be28a.png" title="" alt="image.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/ybm/fadbae38bb/dec85ab5-0273-47b3-ba73-fc0b5f6be28a.png" title="" alt="image.png"></p>
 
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,47 +1,56 @@
 #include <string>
 #include <vector>
+#include <climits>
+#include <iostream>
 #include <queue>
 #include <algorithm>
 
 using namespace std;
 
-vector<vector<int>> graph(20001);
-bool visited[20001] = {false, };
-int solution(int n, vector<vector<int>> edge) {
-    
-    for (auto e : edge) {
+int djikstra(int N, vector<vector<int>> &edge){
+    vector<int> dist(N + 1, INT_MAX);
+    vector<vector<int>> graph(N + 1, vector<int>());
+    for(auto e : edge){
         int a = e[0];
         int b = e[1];
+        
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-
-    queue<pair<int, int>> q;
-    q.push({1, 0}); 
-    visited[1] = true;
-
-    int maxDist = 0;
-    int answer = 0;
-
-    while (!q.empty()) {
-        int node = q.front().first;
-        int dist = q.front().second;
-        q.pop();
-
-        if (dist > maxDist) {
-            maxDist = dist;
-            answer = 1; 
-        } else if (dist == maxDist) {
-            answer++;  
-        }
-
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                visited[next] = true;
-                q.push({next, dist + 1});
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>,  greater<pair<int, int>>> pq;
+    pq.push({0, 1});
+    dist[1] = 0;
+    
+    while(!pq.empty()){
+        int now = pq.top().second;
+        int cost = pq.top().first;
+        pq.pop();
+        
+        for(int next : graph[now]){
+            if(dist[next] > cost + 1){
+                dist[next] = 1 + cost;
+                pq.push({dist[next], next});
             }
         }
     }
+    
+    // 리프 계산
+    sort(dist.begin() + 1, dist.end());
+    
+    int v = dist[N];
+    int result = 0;
+    
+    for(int i = 1; i <= N; i++){
+        if(v == dist[i]) result++;
+    }
+    
+    //for(int i = 1; i <= N; i++) cout << dist[i] << " ";
+    return result;
+}
 
+
+int solution(int n, vector<vector<int>> edge) {
+    int answer = djikstra(n, edge);
     return answer;
 }
